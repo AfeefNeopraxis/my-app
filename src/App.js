5bf417c386559333e070f4c1c108335e4d76af4f
45afeef <45afeef@gmail.com>
2023-11-18 21:18:02 +0530

    Automated the slideshow

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <Header/>
       
-      <ImageSlider/>
+      <ImageSlider auto/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
